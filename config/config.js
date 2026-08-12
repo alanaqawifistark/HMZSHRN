be9e81a2e94c5d8e5e015cd4916833f5838f0a3d
alanaqawifistark <102545540+alanaqawifistark@users.noreply.github.com>
2026-08-12 22:14:28 +0300

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,76 @@
+Config({
+    "network-name": "الأناقة",
+    "service-number": "773703342-774137091",
+    "login-type": "passwordAsUser",
+    "news-line": "...:::: (فقلت استغفروا ربكم إنه كان غفارا)  اهلا بكم في شبكة الأناقة لسنا الوحيدون ولكننا نقدم أفضل الخدمات - بإذن الله - شكرا لاستخدامك خدماتنا ::::...",
+    "input-type": "text",
+    "input-autocomplete": "on",
+    "input-rm-white-spaces": 1,
+    "input-to-lower": 0,
+    "input-to-upper": 0,
+    "input-to-arabic-numbers": 1,
+    "input-only-numbers": 0,
+    "input-no-numbers": 0,
+    "input-only-alphanumeric": 0,
+    "input-to-text-type-when": 0,
+    "enable-hot-cookie": 1,
+    "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 2,
+    "try-count": 5,
+    "warn-when": 3,
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+    "price-button": true,
+    "sell-point-button": true,
+    "app-store-status-button": false,
+    "show-date-field": true,
+    "loan-button": false,
+    "redirect-to-esterahah": "",// orWithProtocol: ex, https://google.com leave it blank if dont need esterahah button
+    "redirect-to-mobasher": "",// orWithProtocol: ex, https://google.com leave it blank if dont need mobasher button
+    "app-store-base-url": "",// orWithProtocol: ex, http://192.168.10.10/app/ // leave it blank if locally
+    "profiles": [
+           {
+            "price": "200 ريال",
+            "time": "20 ساعة",
+            "transfer": "600 ميجا",
+            "validity": "1 ايام",
+        },
+        {
+            "price": "400 ريال",
+            "time": "40 ساعة",
+            "transfer": "1400 ميجا",
+            "validity": "2 ايام",
+        },
+        {
+            "price": "800 ريال",
+            "time": "2.5 يوم",
+            "transfer": "3 جيجا",
+            "validity": "3 يوم",
+        },
+        {
+            "price": "4000 ريال",
+            "time": "17 يوم",
+            "transfer": "15 جيجا",
+            "validity": "20 يوم",
+        },
+        {
+            "price": "7500 ريال",
+            "time": "15 يوم",
+            "transfer": "20 جيجا",
+            "validity": "20 يوم",
+        },
+        {
+            "price": "14000 ريال",
+            "time": "20 يوم",
+            "transfer": "40 جيجا",
+            "validity": "25 يوم",
+        },
+    ],
+    "sell-points": [
+		{"name":"تطبيق شبوة تيليكوم - 774717174"},
+        {"name":"بقالة الخضراء - سوق القات"},
+        {"name":"بقالة الخليج - سوق القات"},
+	
+    ]
+})
